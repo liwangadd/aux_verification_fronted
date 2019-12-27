78934851a9ef4b8d47cd6be8cfd3591c3282230f
kky <kaiyanchn@foxmail.com>
2019-12-27 17:33:07 +0800

    添加了二级分类

--- src/custom/verifyModel/components/entity/entityButtons.vue
+++ src/custom/verifyModel/components/entity/entityButtons.vue
@@ -2,7 +2,7 @@
   <a-row>
     <a-col :span="24" class="button-row">
       <a-button v-for="b in buttonList" :key="b.name" 
-        type="primary" @click="insertAtCursor(b.value)"
+        type="primary" @click="showDetailModel(b)"
         :style="setStyle(b)" >{{b.name }}</a-button>
     </a-col>
   </a-row>
@@ -20,7 +20,67 @@ import { HTMLEncode } from '@/utils/util'
 export default {
   name: "entityButtons",
   props: ["buttonList", "content", "inObj"],
+  data() {
+    return {
+      selectValue: null
+    }
+  },
   methods:{
+
+    // 二级判断
+    showDetailModel(b){
+      const self = this
+      // 判断是否选中
+      const selectText = window.getSelection().toString()
+      if (selectText === ""){
+        return
+      }
+      
+      function setSelectValue(e, value){
+        e.$refs.secondSelect.value = value
+        self.selectValue = value
+      }
+
+      self.selectValue = b.value
+      const model = this.$info({
+          title: '选择二级类型',
+          okText: "确定",
+          content: (
+            <a-select ref="secondSelect" style="width: 200px" maxTagCount={6} value={self.selectValue} onChange={(v) => setSelectValue(this, v)} >
+              <a-select-option key={b.name} value={b.value}> {b.name} </a-select-option>
+              {
+                self.buttonList.map(item => {
+                  if (item == b) return;
+                  return <a-select-option key={b.name} value={item.value}> {item.name} </a-select-option>
+                })
+              }
+            </a-select>
+          ),
+          onOk() {
+            self.insertAtCursor(self.selectValue);
+          },
+          onCancel() {},
+          maskClosable: true,
+          destroyOnClose: true,
+          centered: true,
+          class: "secondSelectModel"
+      });
+
+      // 对模态框的任何多余操作都会导致选中文本失焦，因此直接关闭文本框
+      document.getElementsByClassName("secondSelectModel")[0].addEventListener("click", (e) => {
+        e.preventDefault();
+        model.destroy();
+      });
+
+      document.getElementsByClassName("secondSelectModel")[0].addEventListener("contextmenu",(e) =>{
+        e.preventDefault();
+        model.destroy();
+      });
+
+
+    },
+
+
     // 点击插入
     insertAtCursor(myValue) {
       const self = this
